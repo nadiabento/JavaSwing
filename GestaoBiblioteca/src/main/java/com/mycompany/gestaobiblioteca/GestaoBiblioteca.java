f755af8aebabe216ca8e838d808bd8326bbdde19
nadiabento <nadia-bento@hotmail.com>
2025-04-14 16:06:27 +0100

    teste 4

--- GestaoBiblioteca/src/main/java/com/mycompany/gestaobiblioteca/GestaoBiblioteca.java
+++ GestaoBiblioteca/src/main/java/com/mycompany/gestaobiblioteca/GestaoBiblioteca.java
@@ -4,6 +4,7 @@ package com.mycompany.gestaobiblioteca;
 public class GestaoBiblioteca {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worldgdgfdghkjgkjhjk!");
+        System.out.println("Hello Wasdfasdforld!");
+        System.out.println("teste");
     }
 }
